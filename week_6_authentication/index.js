9d024451b7f068580e47fb7c4e48821b313269ea
Harsh <harshjindal955@gmail.com>
2024-10-15 22:39:01 +0530

    code without using JWT

--- week_6_authentication/index.js
+++ week_6_authentication/index.js
@@ -7,19 +7,19 @@ app.use(express.json());
 let users = [];
 
 // generates a random long string(32 - length)
-// function generateToken(){
-//   let options = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-//   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-//   '!', '@', '#', '$', '%', '^', '&', '*'];
+function generateToken(){
+  let options = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '!', '@', '#', '$', '%', '^', '&', '*'];
 
-//   let token = "";
-//   for(let i = 0; i < 32; i++){
-//     token += options[Math.floor(Math.random() * options.length)]  
-//     //Here the code inside the options will return the random number between the 0 to the length to options whill we declare above in line number 10.
-//   }
-//   return token;
-// }
+  let token = "";
+  for(let i = 0; i < 32; i++){
+    token += options[Math.floor(Math.random() * options.length)]  
+    //Here the code inside the options will return the random number between the 0 to the length to options whill we declare above in line number 10.
+  }
+  return token;
+}
 
 app.post("/signup", function(req, res){
   const username = req.body.username;
