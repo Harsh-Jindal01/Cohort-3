fd57c6dd983a0e734cb8d9f56fbc164ac793d4e3
Harsh <harshjindal955@gmail.com>
2024-10-15 22:40:08 +0530

    code without using JWT

--- week_6_authentication/index.js
+++ week_6_authentication/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 let users = [];
 
-// generates a random long string(32 - length)
+// generates a random long string(32 - length)  .
 function generateToken(){
   let options = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
